2f2f5e872eb0d122aa2daab86ffc9fb3a27da926
shaiksaidhul <157446745+shaiksaidhul@users.noreply.github.com>
2024-05-21 14:42:08 +0530

    level1,2 test

---
+++ dags/level_2_DAG.py
@@ -0,0 +1,60 @@
+# import all modules
+import airflow
+from airflow import DAG
+from datetime import datetime,timedelta
+from airflow.providers.google.cloud.operators.dataproc import (
+    DataprocStartClusterOperator,
+    DataprocStopClusterOperator,
+    DataprocSubmitJobOperator,
+)
+
+# varibale section
+PROJECT_ID="bold-streamer-423605-s6"
+CLUSTER_NAME = "my-dp-cluster"
+REGION = "us-east1"
+JOB_FILE_URI = "gs://source-bkt-data/job_dataproc.py"
+PYSPARK_JOB = {
+    "reference": {"project_id": PROJECT_ID},
+    "placement": {"cluster_name": CLUSTER_NAME},
+    "pyspark_job": {"main_python_file_uri": JOB_FILE_URI},
+}
+
+args = {
+    "owner" : "developer",
+    "start_date" : datetime(2024,5,21),
+    "retries" : 2,
+    "retry_delay" : timedelta(minutes=2)
+}
+
+# define the DAG
+with DAG(
+    "LEVEL_2_DAGg",
+    schedule_interval = '30 7 * * *',
+    default_args = args
+) as dag:
+    
+# define the Tasks
+    start_cluster = DataprocStartClusterOperator(
+        task_id="start_cluster",
+        project_id=PROJECT_ID,
+        region=REGION,
+        cluster_name=CLUSTER_NAME,
+    )
+
+    pyspark_task = DataprocSubmitJobOperator(
+        task_id="pyspark_task", 
+        job=PYSPARK_JOB, 
+        region=REGION, 
+        project_id=PROJECT_ID
+    )
+
+    stop_cluster = DataprocStopClusterOperator(
+        task_id="stop_cluster",
+        project_id=PROJECT_ID,
+        region=REGION,
+        cluster_name=CLUSTER_NAME,
+    )
+
+
+# define the dependency
+start_cluster >> pyspark_task >> stop_cluster
