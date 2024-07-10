ca8e61a5ee056eeca2d34d655d56343c6c77a8b5
shaiksaidhul <157446745+shaiksaidhul@users.noreply.github.com>
2024-07-10 08:05:40 +0530

    new changes test

--- dags/level_1_DAG.py
+++ dags/level_1_DAG.py
@@ -5,51 +5,52 @@ from datetime import datetime, timedelta
 from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
 
-# variable section
-PROJECT_ID = "bold-streamer-423605-s6"
-BUCKET = "source-bkt-data"
-DATASET_NAME_1 = "source_dataset"
-DATASET_NAME_2 = "insight_dataset"
-LOCATION = "US"
-TABLE_NAME_1 = "emp_raw"
-TABLE_NAME_2 = "dep_raw"
-TABLE_NAME_3 = "empDep_in"
-QUERY = f"""
-CREATE TABLE `{PROJECT_ID}.{DATASET_NAME_2}.{TABLE_NAME_3}` as
-SELECT 
-  e.EmployeeID,
-  CONCAT(e.FirstName," ",e.LastName) AS FullName,
-  e.Email,
-  e.Salary,
-  e.JoinDate,
-  d.DepartmentID,
-  d.DepartmentName,
-  cast(e.Salary as integer)*0.01 as Tax
-FROM
-    `{PROJECT_ID}.{DATASET_NAME_1}.{TABLE_NAME_1}` e
-LEFT JOIN
-    `{PROJECT_ID}.{DATASET_NAME_1}.{TABLE_NAME_2}` d
-ON 
-  e.DepartmentID=d.DepartmentID
-"""
 
+# varibale section
+PROJECT_ID = "sigma-rarity-423909-a7"
+BUCKET = "bucket_2_789788945"
+DATASET_NAME_1 = "raw_dataset"
+DATASET_NAME_2 = "insight_dataset"
+TABLE_NAME_1 = "empTable"
+TABLE_NAME_2 = "depTable"
+TABLE_NAME_3 = "empDepTable"
+LOCATION = "US"
+INSERT_ROWS_QUERY = f"""
+CREATE TABLE `{PROJECT_ID}.{DATASET_NAME_2}.{TABLE_NAME_3}` 
+AS
+SELECT DISTINCT
+	e.EmployeeID,
+	CONCAT(e.FirstName," ",e.LastName) as FullName,
+	e.Email,
+	e.Salary,
+	e.JoinDate,
+	CAST(e.salary as integer)*0.01 as TaxComputed,
+	d.DepartmentID,
+	d.DepartmentName	
+FROM
+	`{PROJECT_ID}.{DATASET_NAME_1}.{TABLE_NAME_1}` e
+LEFT JOIN
+	`{PROJECT_ID}.{DATASET_NAME_1}.{TABLE_NAME_2}` d
+ON
+	e.DepartmentID=d.DepartmentID
+"""
 ARGS = {
-    "owner" : "shaik saidhul",
-    "start_date" : datetime(2024,6,5),
+    "owner" : "developer",
+    "start_date" : datetime(2024,7,8),
     "retries" : 2,
     "retry_delay" : timedelta(minutes=2)
 }
 
 
+
 # define the dag
 with DAG(
     "level_1_dag",
-    schedule_interval = "0 15 * * *",
+    schedule_interval = "30 5 * * *",
     default_args = ARGS
 ) as dag:
-
-# define the tasks
     
+# define the tasks
     task_1 = GCSToBigQueryOperator(
         task_id="empTask",
         bucket=BUCKET,
@@ -77,13 +78,13 @@ with DAG(
             {"name": "DepartmentName", "type": "STRING", "mode": "NULLABLE"},
         ],
         write_disposition="WRITE_TRUNCATE",
-    )   
+    )
 
     task_3 = BigQueryInsertJobOperator(
         task_id="empDepTask",
         configuration={
             "query": {
-                "query": QUERY,
+                "query": INSERT_ROWS_QUERY,
                 "useLegacySql": False,
                 "priority": "BATCH",
             }
